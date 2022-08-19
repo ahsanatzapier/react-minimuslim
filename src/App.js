7274b89e55d2e8e0d88093aa0ab26d35ce2815b3
ahsanatzapier <ahsan.hassan@zapier.com>
2022-08-18 19:30:54 -0600

    Before implementing Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.component";
+import SignIn from '../src/components/routes/sign-in/sign-in.component'
+
+const Shop = () =>{
+  return (<h1>hello</h1>)
+}
 
 const App = () => {
   return (
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home /> }/>
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-In' element={<SignIn />}/>
+        </Route>
+    </Routes>
   )
 }
 
---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom"
+import { ReactComponent as MiniMusimLogo } from "../../../assets/MiniMuslim.svg";
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <MiniMusimLogo className="logo"/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>
+            Shop
+          </Link>
+          <Link className="nav-link" to='/sign-in'>
+             Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
