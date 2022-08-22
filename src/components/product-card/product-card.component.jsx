d6c6bc61afff89d4921034a18324e989afb8847f
ahsanatzapier <ahsan.hassan@zapier.com>
2022-08-22 00:12:57 -0600

    Added ability to add items to cart, fixed alert sceme

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,16 @@
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import { useAlert } from "react-alert";
 
 const ProductCard = ({ product }) => {
+  const alert = useAlert();
   const { name, price, imageUrl } = product;
+  const { addItemtoCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemtoCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +18,15 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <Button buttonType="inverted">Add Item</Button>
+      <Button
+        buttonType="inverted"
+        onClick={() => {
+          addItemtoCart(product);
+          alert.success("Item added to cart");
+        }}
+      >
+        Add Item
+      </Button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -10,14 +10,12 @@ import { CartProvider } from "./contexts/cart.context";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
+import AlertTemplate from "../src/template/react-alert.js";
 
 const options = {
-  // you can also just use 'bottom center'
   position: positions.TOP_CENTER,
   timeout: 2000,
   offset: "30px",
-  // you can also just use 'scale'
   transition: transitions.FADE,
 };
 
