be56a4a0fa5b6550278b68d5cf85127f6077fcd7
ahsanatzapier <ahsan.hassan@zapier.com>
2022-08-24 00:14:19 -0600

    Updated shop data

---
+++ src/shop-data.js
@@ -0,0 +1,245 @@
+const SHOP_DATA = [
+  {
+    title: "Hats",
+    items: [
+      {
+        id: 1,
+        name: "Barakah",
+        imageUrl: "https://i.ibb.co/fG3ry7s/barakah.png",
+        price: 12,
+      },
+      {
+        id: 2,
+        name: "Duas",
+        imageUrl: "https://i.ibb.co/vw6n6zB/duas.png",
+        price: 12,
+      },
+      {
+        id: 3,
+        name: "Eid",
+        imageUrl: "https://i.ibb.co/gZ3w5fS/eid.png",
+        price: 12,
+      },
+      {
+        id: 4,
+        name: "Fajr",
+        imageUrl: "https://i.ibb.co/bBnfzns/fajr.png",
+        price: 12,
+      },
+      {
+        id: 5,
+        name: "Oopsie",
+        imageUrl: "https://i.ibb.co/h290g9g/oopsie.png",
+        price: 12,
+      },
+      {
+        id: 6,
+        name: "Ramadan",
+        imageUrl: "https://i.ibb.co/jHY6wxS/ramadan.png",
+        price: 14,
+      },
+      {
+        id: 7,
+        name: "Salam",
+        imageUrl: "https://i.ibb.co/sytQKJ4/salam.png",
+        price: 18,
+      },
+    ],
+  },
+  {
+    title: "Bibs",
+    items: [
+      {
+        id: 8,
+        name: "Bismillah",
+        imageUrl: "https://i.ibb.co/WWrCk0d/bismillah.png",
+        price: 14,
+      },
+      {
+        id: 9,
+        name: "Blessed",
+        imageUrl: "https://i.ibb.co/w6s7VyF/blessed.png",
+        price: 14,
+      },
+      {
+        id: 10,
+        name: "Dua",
+        imageUrl: "https://i.ibb.co/NKB0MJn/dua.png",
+        price: 14,
+      },
+      {
+        id: 11,
+        name: "Feed Me",
+        imageUrl: "https://i.ibb.co/rbmFhxz/feedme.png",
+        price: 14,
+      },
+      {
+        id: 12,
+        name: "Fun",
+        imageUrl: "https://i.ibb.co/HKfMkXz/fun.png",
+        price: 14,
+      },
+      {
+        id: 13,
+        name: "Jalebi",
+        imageUrl: "https://i.ibb.co/qYPmKBF/jalebi.png",
+        price: 14,
+      },
+      {
+        id: 14,
+        name: "Knafeh",
+        imageUrl: "https://i.ibb.co/37HhCKj/knafeh.png",
+        price: 14,
+      },
+      {
+        id: 15,
+        name: "Ladoo",
+        imageUrl: "https://i.ibb.co/TwfLRMZ/ladoo.png",
+        price: 14,
+      },
+      {
+        id: 16,
+        name: "Sabr",
+        imageUrl: "https://i.ibb.co/GJYbYgg/sabr.png",
+        price: 14,
+      },
+      {
+        id: 17,
+        name: "Yummy",
+        imageUrl: "https://i.ibb.co/V9bygcr/yummy.png",
+        price: 14,
+      },
+    ],
+  },
+  {
+    title: "Bodysuits",
+    items: [
+      {
+        id: 18,
+        name: "Alif Ba Ta",
+        imageUrl: "https://i.ibb.co/W50fF9P/alifbata.png",
+        price: 16,
+      },
+      {
+        id: 19,
+        name: "Dua",
+        imageUrl: "https://i.ibb.co/rmB2wMq/dua.png",
+        price: 16,
+      },
+      {
+        id: 20,
+        name: "Eid",
+        imageUrl: "https://i.ibb.co/ykwcp8p/eid.png",
+        price: 16,
+      },
+      {
+        id: 21,
+        name: "Fajr",
+        imageUrl: "https://i.ibb.co/Bc23sNz/fajr.png",
+        price: 16,
+      },
+      {
+        id: 22,
+        name: "Oops",
+        imageUrl: "https://i.ibb.co/wCXLB10/oops.png",
+        price: 16,
+      },
+      {
+        id: 23,
+        name: "Ramadan",
+        imageUrl: "https://i.ibb.co/RvTbKLQ/ramadan.png",
+        price: 16,
+      },
+      {
+        id: 24,
+        name: "Scene",
+        imageUrl: "https://i.ibb.co/GCzDBDn/scene.png",
+        price: 16,
+      },
+      {
+        id: 25,
+        name: "Wow",
+        imageUrl: "https://i.ibb.co/jD81w5h/wow.png",
+        price: 16,
+      },
+    ],
+  },
+  {
+    title: "Pants",
+    items: [
+      {
+        id: 26,
+        name: "Boy Beige",
+        imageUrl: "https://i.ibb.co/j5WYmQB/boybeige.png",
+        price: 10,
+      },
+      {
+        id: 27,
+        name: "Boy Blue",
+        imageUrl: "https://i.ibb.co/vD1YwpQ/boyblue.png",
+        price: 10,
+      },
+      {
+        id: 28,
+        name: "Boy Copper",
+        imageUrl: "https://i.ibb.co/hchv6hq/boycopper.png",
+        price: 10,
+      },
+      {
+        id: 29,
+        name: "Boy Gray",
+        imageUrl: "https://i.ibb.co/0cJwVf1/boygray.png",
+        price: 10,
+      },
+      {
+        id: 30,
+        name: "Boy White",
+        imageUrl: "https://i.ibb.co/zhFHJQ3/boywhite.png",
+        price: 10,
+      },
+      {
+        id: 31,
+        name: "Girl Beige",
+        imageUrl: "https://i.ibb.co/4mQrxRF/girlbeige.png",
+        price: 10,
+      },
+      {
+        id: 32,
+        name: "Girl Purple",
+        imageUrl: "https://i.ibb.co/KVZdY8V/girlpurple.png",
+        price: 10,
+      },
+    ],
+  },
+  {
+    title: "Socks",
+    items: [
+      {
+        id: 33,
+        name: "Blue",
+        imageUrl: "https://i.ibb.co/JW1ryk8/blue.png",
+        price: 6,
+      },
+      {
+        id: 34,
+        name: "Gray",
+        imageUrl: "https://i.ibb.co/k0b0yRM/gray.png",
+        price: 6,
+      },
+      {
+        id: 35,
+        name: "Pink",
+        imageUrl: "https://i.ibb.co/KVqJb1r/pink.png",
+        price: 6,
+      },
+      {
+        id: 36,
+        name: "White",
+        imageUrl: "https://i.ibb.co/jzZ8nfk/white.png",
+        price: 6,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
